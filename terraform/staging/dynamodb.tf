159bf14d9f174315a6c43a8374f9dedfdb483d3c
Charnjit Sohal <charnjit.sohal@news.co.uk>
2021-10-12 09:35:18 +0100

    added CW dashboard

--- terraform/staging/dynamodb.tf
+++ terraform/staging/dynamodb.tf
@@ -1,47 +1,44 @@
 resource "aws_dynamodb_table" "activityhistoryapi_dynamodb_table" {
-    name                  = "ActivityHistory"
-    billing_mode          = "PROVISIONED"
-    read_capacity         = 10
-    write_capacity        = 10
-    hash_key              = "targetId"
-    range_key             = "id"
+  name           = "ActivityHistory"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 10
+  write_capacity = 10
+  hash_key       = "targetId"
+  range_key      = "id"
 
-    attribute {
-        name              = "id"
-        type              = "S"
-    }
+  attribute {
+    name = "id"
+    type = "S"
+  }
 
-    attribute {
-        name              = "targetId"
-        type              = "S"
-    }
+  attribute {
+    name = "targetId"
+    type = "S"
+  }
 
-    attribute {
-        name              = "createdAt"
-        type              = "S"
-    }
+  attribute {
+    name = "createdAt"
+    type = "S"
+  }
 
-    ttl {
-        attribute_name    = "TimetoLiveForRecord"
-        enabled           = true 
-    }
+  ttl {
+    attribute_name = "TimetoLiveForRecord"
+    enabled        = true
+  }
 
-    local_secondary_index {
-        name              = "ActivityHistoryByCreatedAt"
-        range_key         = "createdAt"
-        projection_type   = "ALL"
-    }
+  local_secondary_index {
+    name            = "ActivityHistoryByCreatedAt"
+    range_key       = "createdAt"
+    projection_type = "ALL"
+  }
 
 
-    tags = {
-        Name              = "activity-history-api-${var.environment_name}"
-        Environment       = var.environment_name
-        terraform-managed = true
-        project_name      = var.project_name
-        BackupPolicy     = "Stg"
-    }
+  tags = merge(
+    local.default_tags,
+    { BackupPolicy = "Prod" }
+  )
 
-    point_in_time_recovery {
-        enabled           = true
-    }
+  point_in_time_recovery {
+    enabled = true
+  }
 }
