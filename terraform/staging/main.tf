df2f3f8f401f338e1d988fb0ab482d352258969e
Vladimir Kapenkin <vladimir.kapenkin@gmail.com>
2022-02-04 17:27:50 +0300

    added cloudwatch alarm

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -50,3 +50,16 @@ module "activity_history_api_cloudwatch_dashboard" {
   dynamodb_table_name = aws_dynamodb_table.activityhistoryapi_dynamodb_table.name
   include_sns_widget  = false
 }
+
+data "aws_ssm_parameter" "cloudwatch_topic_arn" {
+  name = "/housing-tl/${var.environment_name}/cloudwatch-alarms-topic-arn"
+}
+
+module "api-alarm" {
+  source           = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/cloudwatch/api-alarm"
+  environment_name = var.environment_name
+  api_name         = "activity-history-api"
+  alarm_period     = "300"
+  error_threshold  = "1"
+  sns_topic_arn    = data.aws_ssm_parameter.cloudwatch_topic_arn.value
+}
