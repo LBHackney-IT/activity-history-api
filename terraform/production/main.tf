3ab7d6b812c327c5f29649b3926164c03348a3b1
thatGui <thatgui@gmail.com>
2025-11-12 17:35:23 +0000

    removed extra whitespace in S3 bucket name for terraform backend

--- terraform/production/main.tf
+++ terraform/production/main.tf
@@ -36,7 +36,7 @@ locals {
 
 terraform {
   backend "s3" {
-    bucket  = "terraform-state-disaster-recovery "
+    bucket  = "terraform-state-disaster-recovery"
     encrypt = true
     region  = "eu-west-2"
     key     = "services/activity-history-api/state"
